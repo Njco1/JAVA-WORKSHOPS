86345031f676d7b522abd23b98fdc2149df4722f
Njco1 <Nicolasaf17@gmail.com>
2024-07-11 19:59:06 -0500

    Avances

--- ARRAYS2.0/src/Ejercicio7.java
+++ ARRAYS2.0/src/Ejercicio7.java
@@ -5,7 +5,22 @@ public class Ejercicio7 {
         
         Scanner scanner = new Scanner(System.in);
 
-        int matriz[][] = {}
+        int [][] matrix = new int[2][3];
+
+                matrix[0][0] = 3;
+                matrix[0][1] = 3;
+                matrix[0][2] = 3; 
+                
+                matrix[1][0] = 1; 
+                matrix[1][1] = 1;
+                matrix[1][2] = 1; 
+        
+                for(int fila = 0; fila < 2; fila++){
+                    System.out.println("Fila "+ fila);
+                    for(int col = 0; col < 3; col++){
+                        System.out.println("Columna "+ col);
+                    }
+                }
 
         scanner.close();
 
